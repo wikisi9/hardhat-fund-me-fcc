c34a0d459079fd5760ac6bd547b39016b4d037b4
DefiJack <wikisi9@gmail.com>
2023-02-23 09:19:25 -0500

    initial commit

---
+++ contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+// importing like this is exactly the same as copying and pasting the entire contract here
